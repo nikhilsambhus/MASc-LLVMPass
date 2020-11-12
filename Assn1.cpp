28ec2a9abd1756956eadb18446fc48d4c8f93d75
Nikhil Sambhus <nikhilsambhus@gmail.com>
2020-11-12 01:30:50 -0500

    Added logic to analyze 2 loops, one nested in other for 2D access analysis along with single loop

--- Assn1.cpp
+++ Assn1.cpp
@@ -7,6 +7,7 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/Pass.h"
 #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/LoopNestAnalysis.h"
 #include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/Support/raw_ostream.h"
 #include "LoopUtils.h"
@@ -118,6 +119,9 @@ namespace {
 	  	StringRef ind;
 		while(nextpos < pathV.size()) {
 			pathElem elem = pathV[nextpos];
+			if(llvm::isa<llvm::GetElementPtrInst> (*elem.inst)) {
+				break;
+			}
 			if(llvm::isa<llvm::PHINode>(*elem.inst)) {
 				Value *v = cast<Value>(elem.inst);
 				ind = v->getName();
@@ -128,27 +132,62 @@ namespace {
 		return ind;
 	  }
 
-	  void analyzePathLoop(pathElems &allPaths, struct LoopData &loopData, llvm::BasicBlock::iterator &inst) {
-	  	StringRef alloc, indVar;
+	  void analyzePathLoop(pathElems &allPaths, std::vector<struct LoopData> &loopDataV, llvm::BasicBlock::iterator &inst) {
+	  	StringRef alloc;
+		std::map<StringRef, int> indVarMap;
 		for(std::vector<pathElem> pathV : allPaths) {
 			unsigned int nextpos = 0;
+			unsigned int allocEptrCnt = 0;
 			for(pathElem elem : pathV) {
 				nextpos++;
 				if(llvm::isa<llvm::GetElementPtrInst> (*elem.inst)) {
 					if(elem.inst->getOperand(0)->getName() == elem.lab) {
-						alloc = checkAlloc(pathV, nextpos);
+						StringRef ret;
+						ret = checkAlloc(pathV, nextpos);
+						if(ret.empty()) {
+							allocEptrCnt++;
+						}
+						else {
+							alloc = ret;
+						}
 					}
 					else if(elem.inst->getOperand(2)->getName() == elem.lab) {
-						indVar = checkInd(pathV, nextpos);
+						StringRef indVar = checkInd(pathV, nextpos);
+						if(allocEptrCnt) {
+							indVarMap[indVar] = 0;
+						}
+						else {
+							indVarMap[indVar] = 1;
+						}
+						
 					}
 				}
 
 			}
 		}
 
-		if(!alloc.empty() && !indVar.empty()) {
-			if(indVar == loopData.indVar) {
-				errs() << *inst << " defines a stream accessing the array " << alloc << " starting from location " << loopData.initV << " going upto " << loopData.finalV << " with the step value of " << loopData.stepV << "\n"; 
+		if(!alloc.empty() && !indVarMap.empty()) {
+			if(indVarMap.size() == 1) { //singple loop logic
+				if(indVarMap.find(loopDataV[0].indVar) != indVarMap.end()) {
+					errs() << *inst << " defines a stream accessing the array " << alloc << " starting from location " << loopDataV[0].initV << " going upto " << loopDataV[0].finalV << " with the step value of " << loopDataV[0].stepV << "\n"; 
+				}
+			}
+			else if(indVarMap.size() == 2) { //nested loop, total 2 loops logic
+				for(struct LoopData loopData : loopDataV) {
+					if(indVarMap.find(loopData.indVar) == indVarMap.end()) {
+						return;
+					}
+				}
+				//induction variables found in paths match the oncs found in loops
+				errs() << *inst << " defines a stream accessing the 2D array " << alloc << " with ";
+				for(std::pair<StringRef, int> indVar : indVarMap) {
+					errs() << indVar.first << " as the " << (indVar.second == 0 ? "first index " : "second index ");
+				}
+				errs() << "\n";
+				for(struct LoopData loopData : loopDataV) {
+					errs() << loopData.indVar << " goes from " << loopData.initV << " to " << loopData.finalV << " with a step value of " << loopData.stepV << " " ;
+				}
+				errs() << loopDataV[1].indVar << " is the inner loop induction variable\n";
 			}
 		}
 
@@ -203,6 +242,37 @@ namespace {
 		  
 		  return allPaths;
 	  }
+
+	  struct LoopData parseLoop(Loop *li, ScalarEvolution &SE) {
+		  struct LoopData loopData;
+		  PHINode *phinode;
+
+		  phinode = li->getInductionVariable(SE);
+		  //errs() << *phinode << "\n";
+
+		  Value* vInd = cast<Value>(phinode);
+		  loopData.indVar = vInd->getName();
+
+		  llvm::Optional<Loop::LoopBounds> lbs = li->getBounds(SE);
+		  Value& vInit = (*lbs).getInitialIVValue();
+		  ConstantInt *Ci;
+		  Ci = cast<ConstantInt>(&vInit);
+		  loopData.initV = Ci->getSExtValue(); 
+		  //errs() << "Initialization of loop is " << loopData.initV << "\n";
+
+		  Value& vFinal = (*lbs).getFinalIVValue();
+		  Ci = cast<ConstantInt>(&vFinal);
+		  loopData.finalV = Ci->getSExtValue(); 
+		  //errs() << "Loop upper bound / goes upto " << loopData.finalV << "\n";
+
+		  Value* step = (*lbs).getStepValue();
+		  Ci = cast<ConstantInt>(step);
+		  loopData.stepV = Ci->getSExtValue(); 
+		  //errs() << "Step value of the loop is " << loopData.stepV << "\n";
+
+		  return loopData;
+
+	  }
 	  bool runOnFunction(Function &F) override {
 		errs() << "\nFunction name : " << F.getName() << "\n";
 		InstCounter = 0;
@@ -241,37 +311,21 @@ namespace {
 
 		LoopCounter = 0;
 		LoopInfo &Li = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-		PHINode *phinode;
 		struct LoopData loopData;
+		ScalarEvolution &SE = getAnalysis<ScalarEvolutionWrapperPass>().getSE(); 
 		for(Loop *lit : Li) {
+			std::vector<struct LoopData> loopDataV;
 			LoopCounter++;
 
-			ScalarEvolution &SE = getAnalysis<ScalarEvolutionWrapperPass>().getSE(); 
+			loopData = parseLoop(lit, SE);		
+			loopDataV.push_back(loopData);
+			std::unique_ptr<LoopNest> lnest = LoopNest::getLoopNest(*lit, SE); 
+			if(lnest->getNumLoops() == 2) {
+				Loop *lin = lnest->getInnermostLoop();
+				loopData = parseLoop(lin, SE);		
+				loopDataV.push_back(loopData);
 
-			phinode = lit->getInductionVariable(SE);
-			//errs() << *phinode << "\n";
-
-			Value* vInd = cast<Value>(phinode);
-			loopData.indVar = vInd->getName();
-
-			llvm::Optional<Loop::LoopBounds> lbs = lit->getBounds(SE);
-			Value& vInit = (*lbs).getInitialIVValue();
-			ConstantInt *Ci;
-			Ci = cast<ConstantInt>(&vInit);
-			loopData.initV = Ci->getSExtValue(); 
-			//errs() << "Initialization of loop is " << loopData.initV << "\n";
-
-			Value& vFinal = (*lbs).getFinalIVValue();
-			Ci = cast<ConstantInt>(&vFinal);
-			loopData.finalV = Ci->getSExtValue(); 
-			//errs() << "Loop upper bound / goes upto " << loopData.finalV << "\n";
-
-			Value* step = (*lbs).getStepValue();
-			Ci = cast<ConstantInt>(step);
-			loopData.stepV = Ci->getSExtValue(); 
-			//errs() << "Step value of the loop is " << loopData.stepV << "\n";
-
-			
+			}
 			for(BasicBlock *BB : lit->getBlocks())
                 	{
                     		//errs() << "basicb name: "<< BB->getName() <<"\n";
@@ -279,7 +333,7 @@ namespace {
 					for (BasicBlock::iterator itr = BB->begin(), e = BB->end(); itr != e; ++itr) {
 						if(llvm::isa <llvm::StoreInst> (*itr) || llvm::isa<llvm::LoadInst> (*itr)) {
 							pathElems allPaths = reverseClosure(itr, defsMap);
-							analyzePathLoop(allPaths, loopData, itr);
+							analyzePathLoop(allPaths, loopDataV, itr);
 						}
 					}
 					
